13f311b4d0119c4ca4454dfe6b818a26a82391d4
Jaime Hurtado <jaime.hurtado@correounivalle.edu.co>
2021-08-19 20:52:47 -0500

    Docker Hub Publish

--- adcws/metapubws/prueba1.py
+++
@@ -1,8 +0,0 @@
-from metapub import PubMedFetcher
-pmids = [2020202, 1745076, 2768771, 8277124, 4031339]
-fetch = PubMedFetcher()
-for pmid in pmids:
-    article = fetch.article_by_pmid(pmid)
-    print(pmid)
-    print(article.title)
-    print(article.citation)
