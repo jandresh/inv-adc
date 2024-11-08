25feb73604a7b2a17619390c7b968c10f41238f0
Jaime Hurtado <jaime.hurtado@correounivalle.edu.co>
2024-11-08 17:47:14 -0500

    preprocessing: adding punk_tab nltk resource

--- preprocessing/app/app.py
+++ preprocessing/app/app.py
@@ -27,6 +27,10 @@ nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 nltk.download("maxent_ne_chunker")
 nltk.download("words")
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('maxent_ne_chunker_tab')
+
 from gensim.summarization import (
     keywords,
 )
